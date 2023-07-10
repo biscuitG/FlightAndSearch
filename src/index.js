b26f2dc1073f2d657e93b333ce1d01b2483b6187
biscuitG <purushottamjha447@gmail.com>
2023-07-10 12:54:17 +0530

    Body-parser enabled

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express= require('express');
+const bodyParser= require('body-parser');
+
+//bodyParser is midlleware basically
 
 const {PORT} = require('./config/serverConfig') 
 const setupAndStartServer = async ()=>{
    // create express object
    const app=express();
+   // Parse JSON bodies
+   app.use(bodyParser.json());
+
+   // Parse URL-encoded bodies
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+   // Now req.body is available for us
 
    app.listen(PORT,()=>{
        console.log(`My server has Started at ${PORT}`);
